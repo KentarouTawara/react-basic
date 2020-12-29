3c8d45415d5c2a5f146b755ecf541166d999b995
KentarouTawara <64579547+KentarouTawara@users.noreply.github.com>
2020-12-29 14:54:51 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import ColorfulMassage from "./components/ColorfulMessage";
+
+const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUP = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColorfulMassage color="blue">オゲンキデスカ？</ColorfulMassage>
+      <ColorfulMassage color="pink">ゲンキです</ColorfulMassage>
+      <button onClick={onClickCountUP}>カウントアップ！</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/of</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>＼(^o^)／</p>}
+    </>
+  );
+};
+
+export default App;
